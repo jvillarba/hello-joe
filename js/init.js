463906cac1a4614d22f20040d0d0075e781e1ddd
Joe V <socagambit@aol.com>
2016-04-14 08:45:41 -0700

    working side nav

--- js/init.js
+++ js/init.js
@@ -3,6 +3,10 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    // Show sideNav
+    $('.button-collapse').sideNav('show');
+    // // Hide sideNav
+    // $('.button-collapse').sideNav('hide');
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
