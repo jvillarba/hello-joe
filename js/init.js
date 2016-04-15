89d202293f5e0f0b19cf007f27fb8b0aefaff034
Joe V <socagambit@aol.com>
2016-04-14 23:51:03 -0700

    cleaning up styling

--- js/init.js
+++ js/init.js
@@ -3,10 +3,6 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    // Show sideNav
-    $('.button-collapse').sideNav('show');
-    // // Hide sideNav
-    // $('.button-collapse').sideNav('hide');
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
